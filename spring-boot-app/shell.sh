d04a41534b43f27f44e62022f5b0883878cc9426
lakshmikanth <lakshmikanthm333@gmail.com>
2025-06-26 22:21:55 +0530

    java

--- spring-boot-app/shell.sh
+++ spring-boot-app/shell.sh
@@ -23,12 +23,12 @@ sudo systemctl start jenkins
 # For Agent
 # "configuring docker"
 # Do this docker config seperately
-sudo yum install -y fontconfig java-21-openjdk
-#install maven aswell if you dont install from pipeline ( sudo yum install -y maven) 
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl enable --now docker
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker ec2-user 
+# sudo yum install -y fontconfig java-21-openjdk
+# #install maven aswell if you dont install from pipeline ( sudo yum install -y maven) 
+# sudo dnf -y install dnf-plugins-core
+# sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+# sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# sudo systemctl enable --now docker
+# sudo systemctl start docker
+# sudo systemctl enable docker
+# sudo usermod -aG docker ec2-user 
